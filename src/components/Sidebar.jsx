2d7666308b73c5981c6584dcfb90add87667163e
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2025-02-07 18:51:51 +0000

    adding Post and Sidebar module

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
+import { PencilLine } from "phosphor-react";
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1672957581550-6b37dcdbf6ff?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://avatars.githubusercontent.com/u/83411249?v=4" />
+
+        <strong>Nicolas Silva</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
